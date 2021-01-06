d1fa9e22b9384ef71c140257f1cbcae086acf2c8
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-06 02:32:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function days() {
+  let day = document.getElementById("answer").value;
+  let age = document.getElementById("input").value;
+  if(day == "Tuesday" && age == "15") {
+  alert("You can enter the museum");
+  }
+}
+
+function index2() {
+  let day = document.getElementById("answer").value;
+  let age = document.getElementById("input").value;
+  if(day == "Friday" && age == "17") {
+  alert("You can enter the museum");
+  }
+}
+
+function index3() {
+  let day = document.getElementById("answer").value;
+  let age = document.getElementById("input").value;
+  if(day == "Monday" && age == "18") {
+  alert("You can enter the museum");
+  }
+}
